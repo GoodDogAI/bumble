2b89c684af3c47915d17648636767151c3775671
Jake (robot) <jakepoz@gmail.com>
2020-02-15 19:43:09 -0800

    Moving the pan tilt randomly, better random distibution code using new C++ style

--- src/randomwalk.cpp
+++ src/randomwalk.cpp
@@ -2,13 +2,23 @@
 #include "std_msgs/String.h"
 #include "std_msgs/Float32.h"
 #include "geometry_msgs/Twist.h"
+#include "dynamixel_workbench_msgs/DynamixelCommand.h"
 
 #include <iostream>
 #include <fstream>
-
+#include <random>
 #include <math.h>
 
 #define MAX_SPEED 0.50
+
+#define TILT_ID 10
+#define PAN_ID 11
+
+std::uniform_real_distribution<> FORWARD_SPEED_DIST(-0.50, 0.50);
+std::uniform_real_distribution<> ANGULAR_RATE_DIST(-0.50, 0.50);
+
+std::uniform_real_distribution<> PAN_DIST(350, 700);
+std::uniform_real_distribution<> TILT_DIST(475, 725);
 
 /**
  * This code just drive the robot around randomly, for the purposes of initializing reinforcement learning training.
@@ -29,21 +39,36 @@ int main(int argc, char **argv)
   ros::NodeHandle n;
 
   ros::Publisher cmd_vel_pub = n.advertise<geometry_msgs::Twist>("cmd_vel", 10);
+  ros::ServiceClient pan_tilt_client = n.serviceClient<dynamixel_workbench_msgs::DynamixelCommand>("/dynamixel_workbench/dynamixel_command");
 
   ros::Rate loop_rate(20);
 
   ros::Time recalc_means = ros::Time::now();
 
-  float forward_mean = 0, forward_var = 0;
-  float angular_mean = 0, angular_var = 0;
+  float forward_mean = 0, angular_mean = 0;
+
+  std::random_device rd;  //Will be used to obtain a seed for the random number engine
+  std::mt19937 gen(rd());
 
   while (ros::ok())
   {
     //Every so often, pick a new mean/variance for the movement parameters
       if (ros::Time::now() - recalc_means > ros::Duration(1)) {
-      forward_mean = MAX_SPEED * (2 * double(rand())/double(RAND_MAX) - 1);
-      angular_mean = MAX_SPEED * (2 * double(rand())/double(RAND_MAX) - 1);
+      forward_mean = FORWARD_SPEED_DIST(gen);
+      angular_mean = ANGULAR_RATE_DIST(gen);
       recalc_means = ros::Time::now();
+
+      dynamixel_workbench_msgs::DynamixelCommand panMsg;
+      panMsg.request.id = PAN_ID;
+      panMsg.request.addr_name = "Goal_Position";
+      panMsg.request.value = PAN_DIST(gen);
+      pan_tilt_client.call(panMsg);
+
+      dynamixel_workbench_msgs::DynamixelCommand tiltMsg;
+      tiltMsg.request.id = TILT_ID;
+      tiltMsg.request.addr_name = "Goal_Position";
+      tiltMsg.request.value = TILT_DIST(gen);
+      pan_tilt_client.call(tiltMsg);
     } 
 
 
@@ -57,7 +82,6 @@ int main(int argc, char **argv)
   ROS_INFO("Sending random direction %f, %f, %f,  Ang %f, %f, %f",
     msg.linear.x, msg.linear.y, msg.linear.z,
     msg.angular.x, msg.angular.y, msg.angular.z);
-
 
 
     ros::spinOnce();
