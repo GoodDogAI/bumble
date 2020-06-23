c27343001963737788647d561cc9e40625570dcb
Jake (robot) <jakepoz@gmail.com>
2020-06-23 13:06:36 -0700

    Working on Cmake settings with custom built pytorch

--- src/brain.cpp
+++ src/brain.cpp
@@ -1,7 +1,72 @@
-#include <torch/torch.h>
-#include <iostream>
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include "std_msgs/Float32.h"
+#include "geometry_msgs/Twist.h"
+#include "dynamixel_workbench_msgs/DynamixelCommand.h"
 
-int main() {
-  torch::Tensor tensor = torch::eye(3);
-  std::cout << tensor << std::endl;
+#include <torch/torch.h>
+
+#include <iostream>
+#include <fstream>
+#include <random>
+#include <math.h>
+
+#define MAX_SPEED 0.50
+
+#define TILT_ID 10
+#define PAN_ID 11
+
+
+
+/**
+ * This code just drive the robot around randomly, for the purposes of initializing reinforcement learning training.
+ */
+int main(int argc, char **argv)
+{
+  /**
+   * You must call one of the versions of ros::init() before using any other
+   * part of the ROS system.
+   */
+  ros::init(argc, argv, "brain");
+
+  /**
+   * NodeHandle is the main access point to communications with the ROS system.
+   * The first NodeHandle constructed will fully initialize this node, and the last
+   * NodeHandle destructed will close down the node.
+   */
+  ros::NodeHandle n;
+
+  ros::Publisher cmd_vel_pub = n.advertise<geometry_msgs::Twist>("cmd_vel", 10);
+  ros::ServiceClient pan_tilt_client = n.serviceClient<dynamixel_workbench_msgs::DynamixelCommand>("/dynamixel_workbench/dynamixel_command");
+
+  ros::Rate loop_rate(20);
+
+  ros::Time recalc_means = ros::Time::now();
+
+  float forward_mean = 0, angular_mean = 0;
+
+  std::random_device rd;  //Will be used to obtain a seed for the random number engine
+  std::mt19937 gen(rd());
+
+  while (ros::ok())
+  {
+    geometry_msgs::Twist msg;
+
+    msg.linear.x = forward_mean;
+    msg.angular.z = angular_mean;
+
+    cmd_vel_pub.publish(msg);
+
+    ROS_INFO("Sending random direction %f, %f, %f,  Ang %f, %f, %f",
+    msg.linear.x, msg.linear.y, msg.linear.z,
+    msg.angular.x, msg.angular.y, msg.angular.z);
+
+
+    ros::spinOnce();
+
+    loop_rate.sleep();
+  }
+
+
+  return 0;
 }
