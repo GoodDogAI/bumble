93ba737853234bf39367cc1fa34de41cff55216d
Jake (robot) <jakepoz@gmail.com>
2020-06-26 16:46:41 -0700

    Loading a YOLO v5 model via torchscript

--- src/brain.cpp
+++ src/brain.cpp
@@ -1,10 +1,10 @@
 #include "ros/ros.h"
-#include "std_msgs/String.h"
-#include "std_msgs/Float32.h"
+#include "sensor_msgs/Image.h"
 #include "geometry_msgs/Twist.h"
 #include "dynamixel_workbench_msgs/DynamixelCommand.h"
 
 #include <torch/torch.h>
+#include <torch/script.h>
 
 #include <iostream>
 #include <fstream>
@@ -16,18 +16,16 @@
 #define TILT_ID 10
 #define PAN_ID 11
 
+ros::Time last_image_received;
 
-struct Net : torch::nn::Module {
-  Net() : linear(register_module("linear", torch::nn::Linear(120, 160))) {
 
-  }
+void cameraImageCallback(const sensor_msgs::ImageConstPtr& img)
+{
+  ROS_INFO("Received camera image with encoding %s, width %d, height %d", 
+  img->encoding.c_str(), img->width, img->height);
 
-  torch::Tensor forward(torch::Tensor input) {
-    return linear(input);
-  }
-
-  torch::nn::Linear linear;
-};
+  last_image_received = ros::Time::now();
+}
 
 /**
  * This code just drive the robot around randomly, for the purposes of initializing reinforcement learning training.
@@ -50,21 +48,35 @@ int main(int argc, char **argv)
   ros::Publisher cmd_vel_pub = n.advertise<geometry_msgs::Twist>("cmd_vel", 10);
   ros::ServiceClient pan_tilt_client = n.serviceClient<dynamixel_workbench_msgs::DynamixelCommand>("/dynamixel_workbench/dynamixel_command");
 
+  ros::Subscriber sub = n.subscribe("/camera/infra2/image_rect_raw", 1, cameraImageCallback);
+
   ros::Rate loop_rate(20);
 
-  ros::Time recalc_means = ros::Time::now();
 
   float forward_mean = 0, angular_mean = 0;
 
   std::random_device rd;  //Will be used to obtain a seed for the random number engine
   std::mt19937 gen(rd());
 
-  Net net;
-torch::Device device = torch::kCPU;
-if (torch::cuda::is_available()) {
-  std::cout << "CUDA is available! Training on GPU." << std::endl;
-  device = torch::kCUDA;
-}
+  torch::Device device = torch::kCPU;
+  if (torch::cuda::is_available()) {
+    std::cout << "CUDA is available! Using the GPU." << std::endl;
+    device = torch::kCUDA;
+  }
+
+  torch::jit::script::Module module;
+  try {
+    // Deserialize the ScriptModule from a file using torch::jit::load().
+    module = torch::jit::load("/home/robot/yolov5s.torchscript");
+    std::cout << "Loaded the model" << std::endl;
+  }
+  catch (const c10::Error& e) {
+    std::cerr << "error loading the model\n";
+    return -1;
+  }
+
+
+
  torch::nn::Sequential discriminator(
   // Layer 1
   torch::nn::Conv2d(
@@ -91,36 +103,19 @@ if (torch::cuda::is_available()) {
   torch::Tensor rand_input = torch::rand({1, 1, 64, 64}).to(device);
   torch::Tensor output = discriminator->forward(rand_input);
 
- 
-  // Time a thousand test runs
-  ros::Time start = ros::Time::now();
+  while (ros::ok())
+  {
+    geometry_msgs::Twist msg;
 
-  for (int i = 0; i < 1000; i++) {
-    rand_input = torch::rand({1, 1, 64, 64}).to(device);
-    output = discriminator->forward(rand_input);
+    msg.linear.x = forward_mean;
+    msg.angular.z = angular_mean;
 
+    cmd_vel_pub.publish(msg);
+
+    ros::spinOnce();
+
+    loop_rate.sleep();
   }
-
-  std::cout << "Time taken (seconds) " << std::endl;
-  std::cout << ros::Time::now() - start << std::endl;
-
-  // while (ros::ok())
-  // {
-  //   geometry_msgs::Twist msg;
-
-  //   msg.linear.x = forward_mean;
-  //   msg.angular.z = angular_mean;
-
-  //   cmd_vel_pub.publish(msg);
-
-  //   ROS_INFO("Sending random direction %f, %f, %f,  Ang %f, %f, %f",
-  //   msg.linear.x, msg.linear.y, msg.linear.z,
-  //   msg.angular.x, msg.angular.y, msg.angular.z);
-
-  //   ros::spinOnce();
-
-  //   loop_rate.sleep();
-  // }
 
 
   return 0;
