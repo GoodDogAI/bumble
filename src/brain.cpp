74c92b53b93269d5b1137440d52c2437d1ee2c98
robot <robot@robot-xavier.localdomain>
2020-07-22 14:42:14 -0700

    Cleaning up code to prepare for nvidia inference engine

--- src/brain.cpp
+++ src/brain.cpp
@@ -5,8 +5,7 @@
 
 #include <boost/make_shared.hpp>
 
-#include <torch/torch.h>
-#include <torch/script.h>
+#include "NvInfer.h"
 
 #include <iostream>
 #include <fstream>
@@ -20,7 +19,6 @@
 
 ros::Time last_image_received;
 
-torch::Tensor image_input = torch::zeros({1, 3, 480, 640}); // N C H W format
 
 std::vector<std::string> yolo_class_names = {
   "person", "bicycle", "car", "motorcycle", "airplane", "bus", "train", "truck", "boat", "traffic light", "fire hydrant", "stop sign",
@@ -38,19 +36,7 @@ void cameraImageCallback(const sensor_msgs::ImageConstPtr& img)
   ROS_INFO("Received camera image with encoding %s, width %d, height %d", 
   img->encoding.c_str(), img->width, img->height);
 
-  // Load the data into a tensor, not that it doesn't take ownership of the ROS message
-  auto temp_tensor = torch::from_blob(const_cast<uint8_t*>(&img->data[0]), 
-                                      {img->height, img->width},
-                                      torch::TensorOptions().dtype(torch::kUInt8));
-
-  // Cast the float in the range 0 to 1                                      
-  temp_tensor = temp_tensor.to(torch::kFloat) / 255.0;
   
-  // Set the dimensions, NCHW format is the standard for pytorch, also expand out to three color dimensions
-  temp_tensor = temp_tensor.view({1, 1, img->height, img->width}).expand({-1, 3, -1, -1});
-
-  image_input = temp_tensor;
-
   last_image_received = ros::Time::now();
 }
 
@@ -86,27 +72,7 @@ int main(int argc, char **argv)
   std::random_device rd;  //Will be used to obtain a seed for the random number engine
   std::mt19937 gen(rd());
 
-  torch::Device device = torch::kCPU;
-  if (torch::cuda::is_available()) {
-    std::cout << "CUDA is available! Using the GPU." << std::endl;
-    device = torch::kCUDA;
-  }
-
-  torch::jit::script::Module yolov5;
-  try {
-    // Deserialize the ScriptModule from a file using torch::jit::load().
-    yolov5 = torch::jit::load("/home/robot/yolov5s.torchscript");
-    std::cout << "Loaded the model" << std::endl;
-    yolov5.to(device);
-    std::cout << "Moved to device" << std::endl;
-  }
-  catch (const c10::Error& e) {
-    std::cerr << "error loading the model\n";
-    return -1;
-  }
-
-//  torch::Tensor rand_input = torch::rand({1, 3, 640, 640}).to(device);
-
+ 
 
   while (ros::ok())
   {
@@ -119,46 +85,7 @@ int main(int argc, char **argv)
 
     ros::Time start = ros::Time::now();
 
-    auto yolo_output = yolov5.forward({image_input.to(device)}).toTuple();
-    auto yolo_features = yolo_output->elements()[0].toTensor().cpu();
-    std::cout << "Yolo ran in " << ros::Time::now() - start << std::endl;
-
-    std::cout << "input shape " << image_input.sizes() << std::endl;
-    std::cout << "yolo shape " << yolo_features.sizes() << std::endl;      
-
-    float threshold = 0.6;
-    auto frame_access = yolo_features.accessor<float,3>();
-
-    auto tagged_output_image = image_input.clone();
-
-    auto frame_bboxes = torch::zeros( {yolo_features.sizes()[1], 4});
-    frame_bboxes.index_put_({torch::indexing::Slice(), 0}, (yolo_features.index({0, torch::indexing::Slice(), 0}) - yolo_features.index({0, torch::indexing::Slice(), 2}) / 2.0));
-    frame_bboxes.index_put_({torch::indexing::Slice(), 1}, (yolo_features.index({0, torch::indexing::Slice(), 1}) - yolo_features.index({0, torch::indexing::Slice(), 3}) / 2.0));
-    frame_bboxes.index_put_({torch::indexing::Slice(), 2}, (yolo_features.index({0, torch::indexing::Slice(), 0}) + yolo_features.index({0, torch::indexing::Slice(), 2}) / 2.0));
-    frame_bboxes.index_put_({torch::indexing::Slice(), 3}, (yolo_features.index({0, torch::indexing::Slice(), 1}) + yolo_features.index({0, torch::indexing::Slice(), 3}) / 2.0));
-    frame_bboxes = frame_bboxes.round().to(torch::kInt32);
-
-    auto bbox_access = frame_bboxes.accessor<int,2>();
-
-    for(int32_t i = 0; i < yolo_features.sizes()[1]; i++) {
-       float base_confidence = frame_access[0][i][4];
-
-       if (base_confidence >= threshold) {
-          for(int32_t class_index = 0; class_index < 80; class_index++) {
-            if( frame_access[0][i][class_index + 5] * base_confidence >= threshold) {
-              std::cout << "Saw class " << yolo_class_names[class_index] << 
-                  " at " << bbox_access[i][0] << "x" << bbox_access[i][1] << " to " << bbox_access[i][2] << "x" << bbox_access[i][3] << std::endl;
-
-              std::cout << frame_access[0][i][0] << std::endl;
-
-              tagged_output_image.index_put_({0, 1, torch::indexing::Slice(bbox_access[i][1], bbox_access[i][3]), torch::indexing::Slice(bbox_access[i][0], bbox_access[i][2])}, 1.0);
-              
-              //This works to just put a green square in a constant place
-              //tagged_output_image.index_put_({0, 1, torch::indexing::Slice(50, 150), torch::indexing::Slice(200, 300)}, 1.0);
-            }
-          }
-       }
-    }      
+   
 
 
     //Copies an image from a tensor back into a ROS Image message and publishes it
@@ -171,7 +98,6 @@ int main(int argc, char **argv)
     yolo_msg->step = 640 * 3;
     size_t size = yolo_msg->step * yolo_msg->height;
     yolo_msg->data.resize(size);
-    memcpy((char*)(&yolo_msg->data[0]), (tagged_output_image * 255).contiguous(torch::MemoryFormat::ChannelsLast).to(torch::kUInt8).data_ptr(), size);
 
     debug_img_pub.publish(yolo_msg);
                           
