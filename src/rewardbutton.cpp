4ea5638687b1e364218410024ff9d37728a2be18
Jake - robot <jakepoz@gmail.com>
2021-07-19 14:50:16 -0700

    Cleaner reward button implementation, still needs polling/select.

--- src/rewardbutton.cpp
+++ src/rewardbutton.cpp
@@ -5,16 +5,8 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <termios.h>
-#include <linux/kd.h>
-#include <linux/keyboard.h>
+#include <linux/input.h>
 #include <sys/ioctl.h>
-
-struct input_event {
-      struct timeval time;
-      unsigned short type;
-      unsigned short code;
-      unsigned int value; 
-};
 
 /**
  * This code listens to a bluetooth device such as a fob, or camera shutter, and 
@@ -40,8 +32,15 @@ int main(int argc, char **argv)
       ROS_ERROR("Could not open input file error %d", fd);
       return 1;
   }
+  
   ROS_INFO("Opened event input file %s", dev_name.c_str());
   
+  // Ros Params for settings rewards/penalties
+  int penalty_keycode = nhPriv.param<int>("penalty_keycode", KEY_ESC);
+  ros::Duration penalty_duration = ros::Duration(nhPriv.param<float>("penalty_duration_secs", 1.0));
+  ros::Time last_penalty;
+
+  // Buffers for reading keyboard IO
   int rd;
   struct input_event ev[16];
 
@@ -51,15 +50,17 @@ int main(int argc, char **argv)
     rd = read(fd, ev, sizeof(ev) * sizeof(input_event));
 
     for (int i = 0; i < rd / sizeof(input_event); i++) {
-      ROS_INFO("keycode %d %d %d", 		
-        ev[i].type,
-        ev[i].code,
-        ev[i].value);
+      // ROS_INFO("keycode %d %d %d", ev[i].type, ev[i].code, ev[i].value);
+
+      if (ev[i].type == EV_KEY && ev[i].code == penalty_keycode && ev[i].value == 1) {
+        ROS_INFO("Penalty keycode detected");
+        last_penalty = ros::Time::now();
+      }
 		}
 
     // Publish the latest value of the reward button
     std_msgs::Float32 reward;
-    reward.data = 0.0;
+    reward.data = (ros::Time::now() - last_penalty) > penalty_duration ? 0.0 : -1.0;
     reward_pub.publish(reward);
 
     ros::spinOnce();
