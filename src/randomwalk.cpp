bfecf764eadb3de890e720702e81be358bbed474
Jake - robot <jakepoz@gmail.com>
2021-07-21 19:01:21 -0700

    Random walk will stop the robot driving if you press the penalty button.

--- src/randomwalk.cpp
+++ src/randomwalk.cpp
@@ -12,6 +12,33 @@
 
 #define DEFAULT_STEPS_PER_DEGREE (1024/300.0)
 
+float external_reward = 0.0f;
+int pan_id = 0;
+int tilt_id = 0;
+
+// Called when you receive a reward message from an external controller
+void rewardButtonCallback(const std_msgs::Float32& rew)
+{
+  if (rew.data != external_reward)
+    ROS_INFO("Received reward button message: %f", rew.data);
+
+  external_reward = rew.data;
+}
+
+void sendDynamixelCommands(ros::ServiceClient &pan_tilt_client, float head_pan, float head_tilt){ 
+   dynamixel_workbench_msgs::DynamixelCommand panMsg;
+    panMsg.request.id = pan_id;
+    panMsg.request.addr_name = "Goal_Position";
+    panMsg.request.value = head_pan;
+    pan_tilt_client.call(panMsg);
+
+    dynamixel_workbench_msgs::DynamixelCommand tiltMsg;
+    tiltMsg.request.id = tilt_id;
+    tiltMsg.request.addr_name = "Goal_Position";
+    tiltMsg.request.value = head_tilt;
+    pan_tilt_client.call(tiltMsg);
+}
+
 /**
  * This code just drive the robot around randomly, for the purposes of initializing reinforcement learning training.
  */
@@ -43,8 +70,14 @@ int main(int argc, char **argv)
   std::uniform_real_distribution<> TILT_DIST(n.param<float>("/pan_tilt/tilt_min_angle", 0.0) * n.param<float>("/pan_tilt/tilt_steps_per_degree", DEFAULT_STEPS_PER_DEGREE),
                                              n.param<float>("/pan_tilt/tilt_max_angle", 90.0) * n.param<float>("/pan_tilt/tilt_steps_per_degree", DEFAULT_STEPS_PER_DEGREE));
 
+  pan_id = n.param<int>("/pan_tilt/pan_id", 1);
+  tilt_id = n.param<int>("/pan_tilt/tilt_id", 2);
+
   ros::Publisher cmd_vel_pub = n.advertise<geometry_msgs::Twist>("cmd_vel", 2);
   ros::Publisher feedback_pub = n.advertise<mainbot::HeadFeedback>("head_feedback", 2);
+
+  ros::Subscriber sub = n.subscribe("/reward_button", 1, rewardButtonCallback);
+
 
   ros::ServiceClient pan_tilt_client = n.serviceClient<dynamixel_workbench_msgs::DynamixelCommand>("/dynamixel_workbench/dynamixel_command");
 
@@ -62,28 +95,29 @@ int main(int argc, char **argv)
   while (ros::ok())
   {
     //Every so often, pick a new mean/variance for the movement parameters
-    if (ros::Time::now() - recalc_means > recalc_freq) {
+    if (ros::Time::now() - recalc_means > recalc_freq && external_reward >= 0.0f) {
       forward_mean = FORWARD_SPEED_DIST(gen);
       angular_mean = ANGULAR_RATE_DIST(gen);
       head_pan = PAN_DIST(gen);
       head_tilt = TILT_DIST(gen);
       recalc_means = ros::Time::now();
 
-      dynamixel_workbench_msgs::DynamixelCommand panMsg;
-      panMsg.request.id = n.param<int>("/pan_tilt/pan_id", 1);
-      panMsg.request.addr_name = "Goal_Position";
-      panMsg.request.value = head_pan;
-      pan_tilt_client.call(panMsg);
-
-      dynamixel_workbench_msgs::DynamixelCommand tiltMsg;
-      tiltMsg.request.id = n.param<int>("/pan_tilt/tilt_id", 2);
-      tiltMsg.request.addr_name = "Goal_Position";
-      tiltMsg.request.value = head_tilt;
-      pan_tilt_client.call(tiltMsg);
+      sendDynamixelCommands(pan_tilt_client, head_pan, head_tilt);
     } 
+    else if (external_reward < 0.0f) {
+      // Stop external motion if you got a negative reward
+      forward_mean = 0.0f;
+      angular_mean = 0.0f;
+
+      head_pan = (PAN_DIST.a() + PAN_DIST.b()) / 2;
+      head_tilt = (TILT_DIST.a() + TILT_DIST.b()) / 2;
+
+      recalc_means = ros::Time::now();
+
+      sendDynamixelCommands(pan_tilt_client, head_pan, head_tilt);
+    }
 
     geometry_msgs::Twist msg;
-
     msg.linear.x = forward_mean;
     msg.angular.z = angular_mean;
 
