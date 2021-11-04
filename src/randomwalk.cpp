1499951d43f9fb17b6e97d3ee0f9485ac835a4bc
Jake Poznanski <jakepoz@gmail.com>
2021-11-03 20:56:08 -0700

    Fixing up the random walk script.

--- src/randomwalk.cpp
+++ src/randomwalk.cpp
@@ -2,18 +2,16 @@
 #include "std_msgs/String.h"
 #include "std_msgs/Float32.h"
 #include "geometry_msgs/Twist.h"
-#include "bumble/HeadFeedback.h"
+#include "bumble/HeadCommand.h"
 
 #include <iostream>
 #include <fstream>
 #include <random>
 #include <math.h>
 
-#define DEFAULT_STEPS_PER_DEGREE (1024/300.0)
 
 float external_reward = 0.0f;
-int pan_id = 0;
-int tilt_id = 0;
+
 
 // Called when you receive a reward message from an external controller
 void rewardButtonCallback(const std_msgs::Float32& rew)
@@ -50,21 +48,18 @@ int main(int argc, char **argv)
   std::uniform_real_distribution<> ANGULAR_RATE_DIST(nhPriv.param<float>("angular_speed_min", -0.50),
                                                      nhPriv.param<float>("angular_speed_max", 0.50));
 
-  std::uniform_real_distribution<> PAN_DIST(n.param<float>("/pan_tilt/pan_min_angle", 0.0) * n.param<float>("/pan_tilt/pan_steps_per_degree", DEFAULT_STEPS_PER_DEGREE),
-                                            n.param<float>("/pan_tilt/pan_max_angle", 90.0) * n.param<float>("/pan_tilt/pan_steps_per_degree", DEFAULT_STEPS_PER_DEGREE));
+  std::uniform_real_distribution<> PITCH_DIST(nhPriv.param<float>("pitch_angle_min", -30.0),
+                                              nhPriv.param<float>("pitch_angle_max", 30.0));
 
-  std::uniform_real_distribution<> TILT_DIST(n.param<float>("/pan_tilt/tilt_min_angle", 0.0) * n.param<float>("/pan_tilt/tilt_steps_per_degree", DEFAULT_STEPS_PER_DEGREE),
-                                             n.param<float>("/pan_tilt/tilt_max_angle", 90.0) * n.param<float>("/pan_tilt/tilt_steps_per_degree", DEFAULT_STEPS_PER_DEGREE));
+  std::uniform_real_distribution<> YAW_DIST(nhPriv.param<float>("yaw_angle_min", -30.0),
+                                            nhPriv.param<float>("yaw_angle_max", 30.0));
 
-  pan_id = n.param<int>("/pan_tilt/pan_id", 1);
-  tilt_id = n.param<int>("/pan_tilt/tilt_id", 2);
 
-  ros::Publisher cmd_vel_pub = n.advertise<geometry_msgs::Twist>("cmd_vel", 2);
+  ros::Publisher cmd_vel_pub = n.advertise<geometry_msgs::Twist>("cmd_vel", 1);
+  ros::Publisher head_pub = n.advertise<geometry_msgs::Twist>("head_cmd", 1);
 
   ros::Subscriber sub = n.subscribe("/reward_button", 1, rewardButtonCallback);
 
-
-  //ros::ServiceClient pan_tilt_client = n.serviceClient<dynamixel_workbench_msgs::DynamixelCommand>("/dynamixel_workbench/dynamixel_command");
 
   ros::Rate loop_rate(20);
 
@@ -72,7 +67,7 @@ int main(int argc, char **argv)
   ros::Duration recalc_freq = ros::Duration(nhPriv.param<float>("change_frequency_secs", 1.0));
 
   float forward_mean = 0, angular_mean = 0;
-  float head_pan = 0, head_tilt = 0;
+  float head_pitch = 0, head_yaw = 0;
 
   std::random_device rd;  //Will be used to obtain a seed for the random number engine
   std::mt19937 gen(rd());
@@ -83,32 +78,31 @@ int main(int argc, char **argv)
     if (ros::Time::now() - recalc_means > recalc_freq && external_reward >= 0.0f) {
       forward_mean = FORWARD_SPEED_DIST(gen);
       angular_mean = ANGULAR_RATE_DIST(gen);
-      head_pan = PAN_DIST(gen);
-      head_tilt = TILT_DIST(gen);
-      recalc_means = ros::Time::now();
+      head_pitch = PITCH_DIST(gen);
+      head_yaw = YAW_DIST(gen);
 
-      // TODO Send to new pan tilt gimbal system
-      //sendDynamixelCommands(pan_tilt_client, head_pan, head_tilt);
+      recalc_means = ros::Time::now();
     } 
     else if (external_reward < 0.0f) {
       // Stop external motion if you got a negative reward
       forward_mean = 0.0f;
       angular_mean = 0.0f;
 
-      head_pan = (PAN_DIST.a() + PAN_DIST.b()) / 2;
-      head_tilt = (TILT_DIST.a() + TILT_DIST.b()) / 2;
+      head_pitch = 0.0f;
+      head_yaw = 0.0f;
 
       recalc_means = ros::Time::now();
-
-      // TODO Send to new pan tilt gimbal system
-      //sendDynamixelCommands(pan_tilt_client, head_pan, head_tilt);
     }
 
     geometry_msgs::Twist msg;
     msg.linear.x = forward_mean;
     msg.angular.z = angular_mean;
-
     cmd_vel_pub.publish(msg);
+
+    bumble::HeadCommand head_msg;
+    head_msg.cmd_angle_pitch = head_pitch;
+    head_msg.cmd_angle_yaw = head_yaw;
+    head_pub.publish(head_msg);
 
     ros::spinOnce();
 
