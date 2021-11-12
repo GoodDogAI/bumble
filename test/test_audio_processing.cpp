e96f44d87a6e51ce8745f4a9f18fc85804e9d42c
Jake Poznanski <jakepoz@gmail.com>
2021-11-11 19:04:26 -0800

    First test case to attempt to replicate running MFCC feature generation in C++

--- test/test_audio_processing.cpp
+++ test/test_audio_processing.cpp
@@ -2,29 +2,54 @@
 
 // Bring in my package's API, which is what I'm testing
 #include "librosa.h"
+#include "npy.hpp"
+
 // Bring in gtest
 #include <gtest/gtest.h>
 
+#include <iostream>
+
+using namespace std;
+
 // Test that you can take a raw mel spectrogram
-TEST(TestSuite, testCase1)
+TEST(TestSuite, testCleanForwardFeatures)
 {
-//       int sr = 16000;
-//   int n_fft = 400;
-//   int n_hop = 160;
-//   std::string window = "hann";
-//   bool center = false;
-//   std::string pad_mode = "reflect";
-//   float power = 2.f;
-//   int n_mel = 40;
-//   int fmin = 80;
-//   int fmax = 7600;
-//   int n_mfcc = 20;
-//   bool norm = true;
-//   int type = 2;
+  int sr = 16000;
+  int n_fft = 512;
+  int n_hop = 160;
+  string window = "hann";
+  bool center = false;
+  string pad_mode = "reflect";
+  float power = 2.f;
+  int n_mel = 64;
+  int fmin = 80;
+  int fmax = 7600;
+  int n_mfcc = 64;
+  bool norm = true;
+  int type = 2;
 
-//     // compute mel spectrogram
-//   std::vector<std::vector<float>> mels = librosa::Feature::melspectrogram(x, sr, n_fft, n_hop, window, center, pad_mode, power,n_mel, fmin, fmax);
+  // Load both the input data and expected resulting features
+  vector<unsigned long> input_shape;
+  bool input_order;
+  vector<float> input_data;
+  npy::LoadArrayFromNumpy("../../src/mainbot/test/clean_forward_input.npy", input_shape, input_order, input_data);
 
+  std::cout << "input_shape " << input_shape[0] << std::endl;
+
+  // Load the expected output
+  vector<unsigned long> output_shape;
+  bool output_order;
+  vector<float> output_data;
+  npy::LoadArrayFromNumpy("../../src/mainbot/test/clean_forward_features.npy", output_shape, output_order, output_data);
+
+   // compute mel spectrogram
+  std::vector<std::vector<float>> mfccs = librosa::Feature::mfcc(input_data, sr,
+                                                                 n_fft, n_hop, window, center, pad_mode,
+                                                                 power, n_mel, fmin, fmax,
+                                                                 n_mfcc, norm, type);
+
+  std::cout << "result shape" << mfccs.size() << " " << mfccs[0].size() << std::endl;
+  std::cout << "expected output shape" << output_shape[0] << " " << output_shape[1] << std::endl;
 }
 
 
