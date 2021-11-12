f1a5ea51d4a0b3c45d6e8c4c914dadd6c4410f0c
Jake Poznanski <jakepoz@gmail.com>
2021-11-11 19:41:38 -0800

    Saving work here, unit test for audio features is not working.

--- test/test_audio_processing.cpp
+++ test/test_audio_processing.cpp
@@ -18,12 +18,12 @@ TEST(TestSuite, testCleanForwardFeatures)
   int n_fft = 512;
   int n_hop = 160;
   string window = "hann";
-  bool center = false;
+  bool center = true;
   string pad_mode = "reflect";
   float power = 2.f;
   int n_mel = 64;
-  int fmin = 80;
-  int fmax = 7600;
+  int fmin = 0;
+  int fmax = 8000;
   int n_mfcc = 64;
   bool norm = true;
   int type = 2;
@@ -42,7 +42,33 @@ TEST(TestSuite, testCleanForwardFeatures)
   vector<float> output_data;
   npy::LoadArrayFromNumpy("../../src/mainbot/test/clean_forward_features.npy", output_shape, output_order, output_data);
 
-   // compute mel spectrogram
+
+  // Duplicate the AmpltiudeToDb behavior of torchaudio, mode "power" with a topDB of 80
+  float topDB = 80.f; 
+  float multiplier = 10.0f;
+  float amin = 1e-10f;
+  float db_multiplier = log10(max(amin, 1.0f));
+  float amax = 0.0f;
+
+  for (int i = 0; i < input_data.size(); i++)
+  {
+    input_data[i] = multiplier * log10(input_data[i] < amin ? amin : input_data[i]);
+    input_data[i] -= multiplier * db_multiplier;
+
+    if (input_data[i] > amax)
+    {
+      amax = input_data[i];
+    }
+  }
+
+  for (int i = 0; i < input_data.size(); i++)
+  {
+    input_data[i] = max(input_data[i], amax - topDB);
+  }
+
+
+
+  // compute mel spectrogram
   std::vector<std::vector<float>> mfccs = librosa::Feature::mfcc(input_data, sr,
                                                                  n_fft, n_hop, window, center, pad_mode,
                                                                  power, n_mel, fmin, fmax,
@@ -50,6 +76,12 @@ TEST(TestSuite, testCleanForwardFeatures)
 
   std::cout << "result shape" << mfccs.size() << " " << mfccs[0].size() << std::endl;
   std::cout << "expected output shape" << output_shape[0] << " " << output_shape[1] << std::endl;
+
+  for (auto i = 0; i < mfccs[0].size(); i++)
+  {
+    std::cout << mfccs[0][i] << std::endl;
+  }
+
 }
 
 
